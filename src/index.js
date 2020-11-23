60be85729f7b866bf253225bffefd015c91eb070
yasuda19 <74058212+yasuda19@users.noreply.github.com>
2020-11-23 11:33:27 +0700

    if reject

--- src/index.js
+++ src/index.js
@@ -6,19 +6,28 @@
 //simulateAsyncAPI("B", 500);
 //simulateAsyncAPI("C", 100);
 
-function simulateAsyncAPI(text, time, callback) {
+function simulateAsyncAPI(text, time) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(text);
+      if (text === "B") reject(new Error("Error!!"));
       resolve();
     }, time);
   });
 }
 
-simulateAsyncAPI("A", 1000)
-  .then(() => {
-    return simulateAsyncAPI("B", 500);
-  })
-  .then(() => {
-    return simulateAsyncAPI("C", 100);
-  });
+//async function run() {
+//await simulateAsyncAPI("A", 1000);
+//await simulateAsyncAPI("B", 500);
+//await simulateAsyncAPI("C", 100);
+//}
+
+//run();
+
+//simulateAsyncAPI("A", 1000)
+//.then(() => {
+//return simulateAsyncAPI("B", 500);
+//})
+//.then(() => {
+//return simulateAsyncAPI("C", 100);
+//});
