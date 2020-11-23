6e153bf03e57bc4f245a0a69764547c27a49b6f5
yasuda19 <74058212+yasuda19@users.noreply.github.com>
2020-11-23 11:07:32 +0700

    simulateAsyncAPI

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@
 //simulateAsyncAPI("C", 100);
 
 function simulateAsyncAPI(text, time, callback) {
-  setTimeout(() => {
-    console.log("text");
-    callback();
-  }, time);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(text);
+      resolve();
+    }, time);
+  });
 }
 
-simulateAsyncAPI("A", 1000, () => {
-  simulateAsyncAPI("B", 500, () => {
-    simulateAsyncAPI("C", 100, () => {});
+simulateAsyncAPI("A", 1000)
+  .then(() => {
+    return simulateAsyncAPI("B", 500);
+  })
+  .then(() => {
+    return simulateAsyncAPI("C", 100);
   });
-});
