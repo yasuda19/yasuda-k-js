40dc28968e22cac8756293e2d6669a8bba27e6b9
yasuda19 <74058212+yasuda19@users.noreply.github.com>
2020-11-23 10:34:12 +0700

    calculation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-function add(a, b) {
-  return a + b;
-}
+// import * as cal from "./calculation";
+// console.log(cal.add(2, 2));
+// console.log(cal.multiply(2, 6));
 
-const add = (a, b) => a + b;
-
-// const add = (a, b) => {
-//   return a + b;
-// }
-
-// const add = (a, b) => a + b
-
-console.log(add(1, 2));
+import * as c from "./calculation";
+console.log(c.add(2, 2));
+console.log(c.multiply(2, 6));
