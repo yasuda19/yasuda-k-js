2b0e0e469b129171d0f865beebe9add3b79bd521
yasuda19 <74058212+yasuda19@users.noreply.github.com>
2020-11-23 10:22:15 +0700

    function add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-let nickname = "your nickname";
-console.log(nickname);
+function add(a, b) {
+  return a + b;
+}
+
+const add = (a, b) => a + b;
+
+// const add = (a, b) => {
+//   return a + b;
+// }
+
+// const add = (a, b) => a + b
+
+console.log(add(1, 2));
